b6aa415dd121b7fc806e13ed243d41234ecabf87
Shaurya Pratap Singh <shaurya.pratap969687@gmail.com>
2026-08-21 10:52:20 +0530

    Basic Program

---
+++ practice.cpp
@@ -0,0 +1,14 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     int rem,rev=0;
+//     cout<<"Enter a number : ";
+//     cin>>n;
+//     while(n!=0){
+//         rem=n%10;
+//         rev=rev*10+rem;
+//         n=n/10;
+//     }
+//     cout<<"Reversed no. : "<<rev;
+// }
--- updateArray.cpp
+++ updateArray.cpp
@@ -4,30 +4,31 @@ int main(){
     int arr[20],n;
     cout<<"Enter number of elements inside array : ";
     cin>>n;
-    cout<<"Enter element inside the array : ";
+    cout<<"Enter elements inside the array : ";
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
     int index,ele;
-    cout<<"Enter element u want to enter : ";
+    cout<<"Enter element u want to insert : ";
     cin>>ele;
     cout<<"Enter position : ";
     cin>>index;
-    
+
     cout<<"Elements before update: ";
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
     }
-    cout<<"After Updatation : ";
-    for(int i=n;i>=index;i--){
-        if(i==index){
+    cout<<endl;
 
-            arr[i]=ele;
-        }
-        
+    // Shift elements right, from the last element up to 'index'
+    for(int i=n; i>index; i--){
+        arr[i] = arr[i-1];
     }
+    arr[index] = ele;
+    n++; // array now has one more element
+
+    cout<<"After insertion : ";
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
     }
-
 }
