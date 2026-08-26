c21a640a3875b31434f792062bd698da1590fb53
Shaurya Pratap Singh <shaurya.pratap969687@gmail.com>
2026-08-27 00:17:08 +0530

    websites

--- practice.cpp
+++ practice.cpp
@@ -1,1 +1,23 @@
+
 #include<iostream>
+using namespace std;
+int main(){
+    int n,sum=0;
+    cin>>n;
+    for(int i=1;i<n;i++){
+        if(n%i==0){
+            sum=sum+i;
+        }
+        else{
+            sum=sum;
+        }
+        
+    }
+    if(sum==n){
+            cout<<"Perfect Number";
+        }
+        else{
+            cout<<"NOT A PERFECT NUMBER";
+        }
+
+}
