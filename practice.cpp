0728de68f4ed558be0f6e32dc65cffb9e757651f
Shaurya Pratap Singh <shaurya.pratap969687@gmail.com>
2026-09-09 10:31:57 +0530

    Program

--- practice.cpp
+++ practice.cpp
@@ -1,23 +1,13 @@
-
 #include<iostream>
 using namespace std;
 int main(){
-    int n,sum=0;
-    cin>>n;
-    for(int i=1;i<n;i++){
-        if(n%i==0){
-            sum=sum+i;
-        }
-        else{
-            sum=sum;
-        }
-        
+    int arr[4];
+    for(int i =0;i<4;i++){
+        cin>>arr[i];
     }
-    if(sum==n){
-            cout<<"Perfect Number";
-        }
-        else{
-            cout<<"NOT A PERFECT NUMBER";
-        }
+    for(int i=0;i<=4;i++){
+        cout<<arr[i]<<" ";
+    }
+
 
 }
